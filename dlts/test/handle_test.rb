135a9ae3a569706f460ee4d34020d3534861db6f
Joseph Pawletko <jgpawletko@gmail.com>
2014-04-03 13:37:40 -0400

    removed extraneous comments from wip_test.rb

--- dlts/test/handle_test.rb
+++ dlts/test/handle_test.rb
@@ -1,17 +1,6 @@
 require 'test_helper'
 class HandleTest < MiniTest::Unit::TestCase
 
-  # it should be invalid with an empty handle file
-  # it should return the proper prefix
-  # it should return the proper suffix
-  # it should be invalid with an invalid handle
-  # it should be invalid if there is more than one handle
-  # handle.to_s
-  # handle.prefix
-  # handle.suffix
-  # handle.to_hdl
-  # handle.to_url
-
   VALID_HANDLE_PATH      = 'test/handle/valid'
   INVALID_HANDLE_PATH    = 'test/handle/invalid'
   EMPTY_HANDLE_PATH      = 'test/handle/empty'
