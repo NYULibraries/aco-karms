cc4c1c40419100b43d327469a315870a07c6381f
Joseph Pawletko <jgpawletko@gmail.com>
2014-04-02 23:47:23 -0400

    snapshot: adding handle and wip tests

--- dlts/test/handle_test.rb
+++ dlts/test/handle_test.rb
@@ -1,5 +1,18 @@
 require 'test/unit'
 
-class WipTest < Test::Unit::TestCase
+class HandleTest < Test::Unit::TestCase
+
+  # it should be invalid with an empty handle file
+  # it should return the proper prefix
+  # it should return the proper suffix
+  # it should be invalid with an invalid handle
+  # it should be invalid if there is more than one handle
+  # handle.to_s
+  # handle.prefix
+  # handle.suffix
+  # handle.to_hdl
+  # handle.to_url
+
+
 
 end
---
+++ dlts/test/wip_test.rb
@@ -0,0 +1,5 @@
+require 'test/unit'
+
+class WipTest < Test::Unit::TestCase
+
+end
