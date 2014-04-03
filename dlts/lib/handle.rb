91043a8548e5887eb2c6de5c313dd3ef215fd9ad
Joseph Pawletko <jgpawletko@gmail.com>
2014-04-03 13:00:43 -0400

    snapshot: handle tests

--- dlts/lib/handle.rb
+++ dlts/lib/handle.rb
@@ -1,7 +1,28 @@
 class Handle
+  attr_reader :handle, :prefix, :suffix
   def initialize(path)
     @path = path.dup
     @errors = {}
+    @handle = @prefix = @suffix = nil
+    process_path!
+  end
+
+  private
+  def process_path!
+    # check that handle file exists and is readable
+    raise "handle file does not exist"   unless File.exists?(@path)
+    raise "handle file must be readable" unless File.readable?(@path)
+
+    # check that handle file only has one line
+    lines = File.read(@path).split("\n")
+    raise "handle file empty or has more than one line"  unless lines.length == 1
+    handle = lines[0]
+
+    # check handle format
+    # ensure that there is only one '/' delimiter
+    match = /\A([^\/]+)\/([^\/]+)\z/.match(handle)
+    raise "incorrect handle format. expecting prefix/suffix"  if match.nil?
+    @handle, @prefix, @suffix = match
   end
 end
 
--- dlts/test/handle_test.rb
+++ dlts/test/handle_test.rb
@@ -15,10 +15,29 @@ class HandleTest < MiniTest::Unit::TestCase
   VALID_HANDLE_PATH   = 'test/handle/valid'
   INVALID_HANDLE_PATH = 'test/handle/invalid'
   EMPTY_HANDLE_PATH   = 'test/handle/empty'
+  DNE_HANDLE_PATH     = 'this/path/does/not/exist'
 
-  def test_valid_invocation
+  def test_class
     assert_instance_of(Handle, Handle.new(VALID_HANDLE_PATH))
   end
 
+  def test_valid
+    assert(Handle.new(VALID_HANDLE_PATH))
+  end
+
+  def test_empty_handle_file
+    err = assert_raises(RuntimeError) { Handle.new(EMPTY_HANDLE_PATH) }
+    assert_match(/handle file empty or has more than one line/, err.message)
+  end
+
+  def test_invalid_handle_file
+    err = assert_raises(RuntimeError) { Handle.new(INVALID_HANDLE_PATH) }
+    assert_match(/incorrect handle format/, err.message)
+  end
+
+  def test_nonexistent_handle_file
+    err = assert_raises(RuntimeError) { Handle.new(DNE_HANDLE_PATH) }
+    assert_match(/handle file does not exist/, err.message)
+  end
 
 end
