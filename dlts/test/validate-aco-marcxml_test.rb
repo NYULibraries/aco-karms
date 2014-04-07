52ef7d80da7192c1747c2f2d63fed0c3c9126fe2
Joseph Pawletko <jgpawletko@gmail.com>
2014-04-07 10:47:32 -0400

    multiple valid working

--- dlts/test/validate-aco-marcxml_test.rb
+++ dlts/test/validate-aco-marcxml_test.rb
@@ -45,31 +45,17 @@ class ValidateAcoMarcxml < MiniTest::Unit::TestCase
     assert_match(/File is not readable/, e)
   end
 
+  def test_multiple_valid_files
+    o, e, s = Open3.capture3("#{COMMAND} #{VALID_MARCXML_PATH} #{VALID_DEFAULT_NS_PATH} #{VALID_NON_DEFAULT_NS_PATH}")
+    assert(s == 0, "exit status: #{e}")
+    assert("processed: 3 valid: 3 invalid: 0\n" == o, "stdout was: '#{o}'")
+    assert('' == e, "stderr")
+  end
+
+#  def test_multiple_valid_and_invalid_files
 
   # restore read/write permissions on test file
   MiniTest::Unit.after_tests { File.chmod( 0644, UNREADABLE_MARCXML_PATH) }
 
 
-=begin
-  def test_with_unwritable_dir
-    o, e, s = Open3.capture3("#{COMMAND} #{UNWRITABLE_DIR} #{NNC_V1}")
-    assert(s != 0)
-    assert(o == '')
-    assert_match(/usage/, e)
-    assert_match(/bad target directory/, e)
-  end
-
-  def test_with_non_existent_wip_dir
-    o, e, s = Open3.capture3("#{COMMAND} #{WORK_DIR} #{DNE_PATH}")
-    assert(s != 0)
-    assert(o == '')
-    assert_match(/directory does not exist/, e)
-  end
-
-  MiniTest::Unit.after_tests do
-    FileUtils.mkdir(WORK_DIR) unless File.exists?(WORK_DIR)
-    FileUtils.touch(File.join(WORK_DIR, '.gitkeep'))
-  end
-
-=end
 end
