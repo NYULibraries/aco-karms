4e329776f11d3e3fb89a81330b3fe3baec0f0a98
Joseph Pawletko <jgpawletko@gmail.com>
2014-04-07 10:59:29 -0400

    finished validate-aco-marcxml tests, fixed exitstatus checks in gen-work-dirs_test.rb

--- dlts/test/validate-aco-marcxml_test.rb
+++ dlts/test/validate-aco-marcxml_test.rb
@@ -24,7 +24,7 @@ class ValidateAcoMarcxml < MiniTest::Unit::TestCase
 
   def test_with_incorrect_argument_count
     o, e, s = Open3.capture3("#{COMMAND}")
-    assert(s != 0)
+    assert(s.exitstatus != 0)
     assert(o == '')
     assert_match(/usage/, e)
     assert_match(/incorrect number of arguments/, e)
@@ -32,7 +32,7 @@ class ValidateAcoMarcxml < MiniTest::Unit::TestCase
 
   def test_with_non_existent_file
     o, e, s = Open3.capture3("#{COMMAND} #{DNE_MARCXML_PATH}")
-    assert(s != 0)
+    assert(s.exitstatus != 0)
     assert(o == '')
     assert_match(/File does not exist/, e)
   end
@@ -40,22 +40,29 @@ class ValidateAcoMarcxml < MiniTest::Unit::TestCase
   def test_unreadable_marcxml_file
     File.chmod( 0000, UNREADABLE_MARCXML_PATH)
     o, e, s = Open3.capture3("#{COMMAND} #{UNREADABLE_MARCXML_PATH}")
-    assert(s != 0)
+    assert(s.exitstatus != 0)
     assert(o == '')
     assert_match(/File is not readable/, e)
   end
 
   def test_multiple_valid_files
     o, e, s = Open3.capture3("#{COMMAND} #{VALID_MARCXML_PATH} #{VALID_DEFAULT_NS_PATH} #{VALID_NON_DEFAULT_NS_PATH}")
-    assert(s == 0, "exit status: #{e}")
+    assert(s.exitstatus == 0, "exit status: #{s}")
     assert("processed: 3 valid: 3 invalid: 0\n" == o, "stdout was: '#{o}'")
     assert('' == e, "stderr")
   end
 
-#  def test_multiple_valid_and_invalid_files
+  def test_multiple_valid_and_invalid_files
+    args = "#{VALID_MARCXML_PATH} #{INVALID_001_PATH} " +
+      "#{VALID_DEFAULT_NS_PATH} #{INVALID_003_PATH} " +
+      "#{VALID_NON_DEFAULT_NS_PATH}"
+    o, e, s = Open3.capture3("#{COMMAND} #{args}")
+    assert(s.exitstatus == 1, "exit status: #{s}")
+    assert("processed: 5 valid: 3 invalid: 2\n" == o, "stdout was: '#{o}'")
+    assert_match(/missing controlfield 003/, e, "stderr: #{e}")
+    assert_match(/missing controlfield 001/, e, "stderr: #{e}")
+  end
 
   # restore read/write permissions on test file
   MiniTest::Unit.after_tests { File.chmod( 0644, UNREADABLE_MARCXML_PATH) }
-
-
 end
