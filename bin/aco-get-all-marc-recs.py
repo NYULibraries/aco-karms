b857bef7b21bdcf09fe1caa28ed4db6f3fecc820
aprovoNYU <alexandra.provo@nyu.edu>
2020-07-08 12:01:27 -0400

    Process fix to two records - UaCaAUL_20190929 - DLTSACO-791

--- bin/aco-get-all-marc-recs.py
+++ bin/aco-get-all-marc-recs.py
@@ -11,6 +11,16 @@ from pymarc import Record, Field
 import aco_globals
 import aco_functions
 
+def wrapper(gen):
+  while True:
+    try:
+      yield next(gen)
+    except StopIteration:
+      break
+    except Exception as e:
+      print(e) # or whatever kind of logging you want
+
+
 aco_mrc_all = pymarc.MARCWriter(file(aco_globals.work_folder+'/'+'mrc_out_all-3.mrc', 'w'))
 
 # Retrieve individual final MARC files from each mrc_out batch folder
@@ -22,7 +32,7 @@ for root, folders, files in os.walk(aco_globals.work_folder):
 				for mrc_file in files:
 					mrc_file_path = os.path.join(root, mrc_file)
 					this_mrc_files = pymarc.MARCReader(file(mrc_file_path), to_unicode=True, force_utf8=True)
-					for this_mrc_file in this_mrc_files:
+					for this_mrc_file in list(wrapper(this_mrc_files)):
 						this_mrc_003 = this_mrc_file.get_fields('003')[0].value()
 						this_mrc_001 = this_mrc_file.get_fields('001')[0].value()
 						print '('+this_mrc_003+')'+this_mrc_001
